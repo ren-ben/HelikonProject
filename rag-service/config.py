b7c0e9dcae00986e5d86987760096a181cbefde9
a401401 <j9702318@gmail.com>
2026-02-14 21:07:54 +0100

    change config to use local llms

--- rag-service/config.py
+++ rag-service/config.py
@@ -1,14 +1,18 @@
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
     llm_provider: str = "openai"
     llm_api_key: str = ""
     llm_model: str = "gpt-4o-mini"
+
+    ollama_url: str = "http://host.docker.internal:11434"
+
     embedding_api_key: str = ""
-    chromadb_path: str = "/data/chromadb"
 
-    model_config = {"env_file": ".env"}
+    chromadb_path: str = "./data/chromadb"
 
+    class Config:
+        env_file = ".env"
+        case_sensitive = False
 
 settings = Settings()
